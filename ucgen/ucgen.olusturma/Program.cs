0633ac767a23d4381d76596bcdf5dce6a709ebca
İrem Su Kurtkaya <iremsukurtkaya@gmail.com>
2022-04-13 23:07:48 +0300

    ucgen olusturma
    for dongusu ile

---
+++ ucgen/ucgen.olusturma/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ucgen.olusturma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("yükseklik:");
+            int yukseklık = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= yukseklık; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    //Boşluk koyarak üçgenin şeklini değiştirebilirsiniz.
+                    Console.Write("  *  ");
+                    Console.Write("  ");
+                    //Dik üçgen
+                    //Console.Write("*");
+                    // Console.Write("  ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
